4f1c18607581863b4eab91e32fcb9f90e4f19ec8
Lubni Morais <lubni.morais@gmail.com>
2020-09-10 12:13:52 -0300

    Corrected import UpdateProductService in ProductControlle

--- src/modules/product/infra/http/controllers/ProductController.ts
+++ src/modules/product/infra/http/controllers/ProductController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
-import UpdateProductservice from '../../../services/UpdateProductService';
+import UpdateProductService from '../../../services/UpdateProductService';
 
 export default class ProductController {
     public async update(
@@ -11,7 +11,7 @@ export default class ProductController {
         const { customer_url } = request.params;
         const { id, name, price, category_id, description } = request.body;
 
-        const updateProductService = container.resolve(UpdateProductservice);
+        const updateProductService = container.resolve(UpdateProductService);
 
         const menu = await updateProductService.execute({
             customer_url,
