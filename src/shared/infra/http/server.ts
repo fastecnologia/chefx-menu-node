8495bec8391fed2034796d82824af07d53300ca9
Lubni Morais <lubni.morais@gmail.com>
2024-08-08 16:13:38 -0300

    fix: fix size request body

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -5,6 +5,8 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
+import bodyParser from 'body-parser';
+
 import uploadConfig from '../../../config/upload';
 
 import routes from './routes';
@@ -19,6 +21,9 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 app.use('/image_product', express.static(uploadConfig.tmpFolder));
+
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
