d23a2e8e7dd98c8109c2439434be001bb00f5645
Lubni Morais <lubni.morais@gmail.com>
2025-01-16 16:26:45 -0300

    feat: delete image product when delete menu

--- src/modules/menu/services/DeleteMenuService.ts
+++ src/modules/menu/services/DeleteMenuService.ts
@@ -1,13 +1,21 @@
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '../../../shared/errors/AppError';
+
+import { IStorageProvider } from '../../../shared/container/providers/StorageProvider/models/IStorageProvider';
+
 import IMenuRepository from '../repositories/IMenuRepository';
+
+import { IProductJSON } from '../../product/dtos/IProductDTO';
 
 @injectable()
 class DeleteMenuService {
   constructor(
     @inject('MenuRepository')
     private menuRepository: IMenuRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
 
   public async execute(customer_url: string): Promise<void> {
@@ -15,6 +23,17 @@ class DeleteMenuService {
 
     if (!menu) {
       throw new AppError('Menu not exists');
+    }
+
+    const convertStringToJSON = JSON.stringify(menu.products);
+    const productJSONArray = JSON.parse(convertStringToJSON) as Array<
+      IProductJSON
+    >;
+
+    for await (const product of productJSONArray) {
+      if (product.image) {
+        await this.storageProvider.deleteFile(product.image, customer_url);
+      }
     }
 
     await this.menuRepository.delete(menu.id);
